6e134c02f7e5c266cc14331b815eb6728a511be5
Ken McGrady <ken.mcgrady@gmail.com>
2016-05-24 16:49:26 -0700

    Add Assignment questions

--- part1/assignment.js
+++ part1/assignment.js
@@ -1,332 +1,138 @@
-// Define a function named sum that takes two arguments
-//    a (number)
-//    b (number)
+// Define a function lengths that takes in one argument.
+//     arr (array of strings)
 //
-// Return the sum of these two arguments
-function sum(a, b) {
-  // return a + b;
-}
-
-// Define a function named product that takes two arguments
-//    a (number)
-//    b (number)
-//
-// Return the product of these two arguments
-function product(a, b) {
-  // YOUR CODE HERE
-}
-
-// Define a function named sumAndProduct that takes three arguments
-//    x (number)
-//    y (number)
-//    z (number)
-//
-// Return the sum of the first two arguments multiplied by the third argument.
-// For example, given 1, 2, and 5, then return 15.
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Grouping
-function sumAndProduct(x, y, z) {
+// It returns a new array of numbers where each element is the length (number
+// of characters) of the corresponding element in the input. For example:
+//   lengths(['Kenneth', 'Ian', 'Ryan']) will output [7, 3, 4].
+function lengths(arr) {
 
 }
 
-// Define a function named roundUp that takes one argument
-//    decimal (number)
+// Define a function named max that takes in one argument.
+//    arr (array of numbers)
 //
-// Return the argument rounded up to the nearest integer
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/ceil
-function roundUp(decimal) {
-
-}
-
-// Define a function named toFahrenheit that takes one argument
-//    celsius (number)
-//
-// Return the argument converted to Fahrenheit rounded to the nearest integer.
-// Use Google to find the formula.
-
-
-
-// Define a function named areaOfCircle that takes one argument
-//    radius (number)
-//
-// Return the area of a circle using that argument. Use Google to find the
-// formula.
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI
-
-
-
-// Define a function named areaOfRing that takes two arguments
-//    outerRadius (number)
-//    innerRadius (number)
-//
-// Return the area of a circular ring using these arguments. Use Google to find
-// the formula.
-
-
-
-// Define a function named greet that takes 2 arguments
-//    firstName (string)
-//    lastName (string)
-//
-// Return a string in the format 'Hello, Kelly Rippa!' using these arguments
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Template_strings
-
-
-
-// Define a function named toSentence that takes four arguments
-//    word1 (string)
-//    word2 (string)
-//    word3 (string)
-//    oxfordComma (boolean)
-//
-// If oxfordComma is true,
-//    Return a string in the format "word1, word2, and word3."
-// If oxfordComma is false,
-//    Return a string in the format "word1, word2 and word3."
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Addition
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#String_operators
-
-
-
-// Define a function named toRoman that takes one argument
-//    arabic (number)
-//
-// If the argument is less than 1
-//    Return null
-// If the argument is greater than 10
-//    Return null
-// Otherwise
-//    Return the argument converted to a roman numeral string
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling#switch_statement
-
-
-
-// Define a function toDolla that takes one argument
-//    amount (number)
-//
-// Return the argument converted to a dollar currency string. For example, given
-// 10, then return '$10.00'.
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-
-
-
-// Define a function named percentage that takes two arguments
-//    numerator (number)
-//    denominator (number)
-//
-// Return the percentage of the first argument divided by the second. For
-// example, given 1 and 4, then return '25.0%'.
-
-
-
-// Define a function named isStrictlyEqual that takes two arguments
-//    value1 (anything)
-//    value2 (anything)
-//
-// If the two arguments are both equal and have the same type
-//    Return true
-// Otherwise
-//    Return false
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#Strict_equality_using
-//
-// See https://dorey.github.io/JavaScript-Equality-Table/
-
-
-
-// Define a function named isLooselyEqual that takes two arguments
-//    value1 (anything)
-//    value2 (anything)
-//
-// If the two arguments are both equal regardless of type
-//    Return true
-// Otherwise
-//    Return false
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#Loose_equality_using
-
-
-
-// Define a function named remainder that takes two arguments
-//    numerator (number)
-//    denominator (number)
-//
-// Return the remainder of the first argument divided by the second. For
-// example, given 4 and 3, then return 1.
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder_()
-
-
-
-// Define a function named isEven that takes one argument
-//    integer (number)
-//
-// If the number is even
-//    Return true
-// Otherwise
-//    Return false
-//
-// Hint: What number will give you a remainder of 0 if the argument is even?
-
-
-
-// Define a function named isOdd that takes one argument
-//    integer (number)
-//
-// If the number is odd
-//    Return true
-// Otherwise
-//    Return false
-
-
-
-// Define a function named isVowel that takes one argument
-//    letter (string)
-//
-// If the letter is a vowel
-//    Return true
-// Otherwise
-//    Return false
-//
-// Treat 'y' as a consonant.
-
-
-
-// Define a function named largestOfThree which takes three arguments
-//    value1 (number)
-//    value2 (number)
-//    value3 (number)
-//
-// Return the largest argument by value
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-
-
-
-// Define a function named longestOfThree which takes three arguments
-//    value1 (string)
-//    value2 (string)
-//    value3 (string)
-//
-// Return the longest argument by length
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length
+// Return the maximum number in the array. For example, given [1, 2, -3, 4],
+// then return 4. If the array is empty, return -Infinity.
 //
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
 
 
-
-// Define a function named iceCreamPosition that takes two arguments
-//    pieTemperature (string)
-//    iceCreamFlavor (string)
+// Define a function named filterPassingGrades that takes in one argument.
+//     grades (array of numbers)
 //
-// If iceCreamFlavor is 'cardamom'
-//    Return 'not at all' immediately
-// Otherwise
-//    If pieTemperature is 'cold'
-//      Return 'on top'
-//    If pieTemperature is 'warm'
-//      Return 'on the side'
-// Otherwise
-//    Return 'up to you'
+// Return a new array with any grade less than 70 filtered out. For example,
+// given [88, 67, 70, 92, 53], then return [88, 70, 92].
 
 
-
-// Define a function named isLeapYear that takes one argument
-//    year (number)
+// Define a function named min that takes in one argument.
+//    arr (array of numbers)
 //
-// If the argument is a leap year
-//    Return true
-// Otherwise
-//    Return false
+// Return the minimum number in the array. For example, given [1, 2, -3, 4],
+// then return -3. If the array is empty, return Infinity.
 //
-// See: https://en.wikipedia.org/wiki/Leap_year#Algorithm
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
 
 
-
-// Define a function named shout that takes one argument
-//    message (string)
+// Define a function named pluck that takes in two arguments.
+//     arr (array of objects)
+//     key (string)
 //
-// Return the same argument but all in uppercase letters. For example, given
-// 'Shut the front door', then return 'SHUT THE FRONT DOOR'
+// Return a new array where each element is the keyed value of each object.
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-
-
-
-// Define a function named whisper that takes one argument
-//    message (string)
+// For example, given the array:
+//    [
+//      { name: 'moe', age: 40 },
+//      { name: 'larry', age: 50 },
+//      { name: 'curly', age: 60 }
+//    ]
 //
-// Return the same argument but all in lowercase letters and prefixed by
-// 'shhh... '. For exaple, given 'GOLF TIME', then return "shhh... golf time"
+// and the key 'name', then return ['moe', 'larry', 'curly'].
+
+
+// Define a function onlyEvens which takes in one argument:
+//    arr (array of numbers)
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// It returns an array only containing the numbers that are even. For example,
+// given the input, [1, 2, 3, 4, 5], the function would produce [2, 4].
 
 
+// ----------------------------------------------------------------------------
+// For the questions below, you will be assuming the results are of the form
+// coming from the Yelp API. See the file in this directory called
+// yelp_search_results.json for an example API response.
+// ----------------------------------------------------------------------------
 
-// Define a function named stopAt that takes two arguments
-//   source (string)
-//   stop (string)
+// Define a function named businessNamesAndRatings, which takes in one argument
+//    result (Yelp API search result)
 //
-// Return the source all the way up to the position where stop begins. For
-// example, given 'how now brown cow' and 'brown', then return 'how now'.
+// The function will return an array of objects of the following format:
+// {
+//   name: BUSINESS_NAME,
+//   rating: RATING
+// }
+// The name of the business and the rating will be the same name and rating
+
+
+// Define a function named averageRating, which takes in one argument:
+//    result (Yelp API search result)
 //
-// Hint: This will require a combination of 3 different steps
+// The function will return a number, which is the average rating of the
+// businesses in the result set. If there are no businesses, return null.
+
+
+// Define a function named aroundMe, which takes in two arguments:
+//    result     (Yelp API search result)
+//    coordinate (object of the following format: {lat: NUMBER, lon: NUMBER})
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+// The function will return an array of businesses who is within 1 mile of your
+// coordinate.
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+// NOTE: We have provided a function that computes the distance between two
+// earth coordinates (in the format {lat: NUMBER, lon: NUMBER}).
+function earthDistance(coord1, coord2) {
+  var RADIUS_OF_EARTH = 3961; // miles
+  var lat1 = coord1.lat * Math.PI / 180;
+  var lat2 = coord2.lat * Math.PI / 180;
+  var lon1 = coord1.lon * Math.PI / 180;
+  var lon2 = coord2.lon * Math.PI / 180;
+
+  var dlon = lon2 - lon1;
+  var dlat = lat2 - lat1;
+
+  var a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) *
+    Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return RADIUS_OF_EARTH * c;
+}
+
+
+// Define a function named businessRatingTable, which takes in one argument
+//    result (Yelp API search result)
 //
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
-
-
-
-// Define a function named capitalize that takes one argument
-//    message (string)
+// The function will return a table DOM element of the following format:
+// <table>
+//   <thead>
+//     <tr>
+//       <th>Name</th>
+//       <th>Rating</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <td>BUSINESS_NAME</td>
+//       <td>RATING</td>
+//     </tr>
+//     <tr>
+//       <td>BUSINESS_NAME</td>
+//       <td>RATING</td>
+//     </tr>
+//     <tr>
+//       <td>BUSINESS_NAME</td>
+//       <td>RATING</td>
+//     </tr>
+//     ...
+//   </tbody>
+// </table>
 //
-// Return the argument with the first letter capitalized. For example, given
-// 'oh, you', then return 'Oh, you'.
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
-
-
-
-// Define a function named leftPad5 that takes one argument
-//    word (string)
-//
-// If the argument's length is less than 5
-//    Return the argument but prefixed with spaces until it's 5 characters long
-// If the argument's length is 5 or more characters
-//    Return the argument
-
-
-
-// Define a function named superPicky that takes one argument
-//    value (anything)
-//
-// If the argument is a string,
-//    Return 'Thanks! Got it.'
-// Otherwise
-//    Return 'I wanted a string, but all I got was a stinking TYPE'
-//
-// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
-
-
-
-// Define a function named calculateTaxRate that takes two arguments
-//    salary (number from 1 to 74,900)
-//    status (string that's either 'single' or 'joint')
-//
-// If the status is anything other than 'single' or 'joint'
-//    Return a string that says 'Better call an accountant'
-// If the salary is greater than 74,900
-//    Return a string that says 'Better call an accountant'
-// Otherwise
-//    Return the correct tax rate as a string using the table from http://www.efile.com/tax-service/tax-calculator/tax-brackets/
+// NOTE: You may use jQuery if you like, but the return type is still a native
+// DOM element.
